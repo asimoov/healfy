01c1a238c42e03dbeb27688d39fb31b69c77f689
Denis Rangel <denis.rangel@gmail.com>
2014-03-30 12:14:30 -0300

    feature menu active

--- public/scripts/views/layout.js
+++ public/scripts/views/layout.js
@@ -9,9 +9,27 @@ define([
 
 	return Backbone.View.extend({
 		template: Handlebars.compile(layout),
+		initialize: function() {
+			this.listenTo(Backbone.history, 'route', this.activeMenu, this);
+		},
 		render: function() {
 			this.$el.empty();
 			this.$el.append(this.template());
+		},
+		activeMenu: function() {
+			if(Backbone.history.getHash() === "") {
+				$('.nav li').removeClass('active');
+				$('.nav li#home').addClass('active');
+			} else if(Backbone.history.getHash() === "affiliates") {
+				$('.nav li').removeClass('active');
+				$('.nav li#affiliates').addClass('active');
+			} else if(Backbone.history.getHash() === "providers") {
+				$('.nav li').removeClass('active');
+				$('.nav li#providers').addClass('active');
+			} else if(Backbone.history.getHash() === "patients") {
+				$('.nav li').removeClass('active');
+				$('.nav li#patients').addClass('active');
+			}
 		}
 	});
 });
